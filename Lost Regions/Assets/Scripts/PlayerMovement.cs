74bced8d4c93159ec08d09bb6ed4036ff2e5e391
alex4101 <72837778+alex4101@users.noreply.github.com>
2022-04-12 17:57:51 -0700

    Character Movement + Mouse Camera Rotation
    Character Imported. Imported animations. Added movement and mouse camera rotation

---
+++ Lost Regions/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Variables
+    public CharacterController controller;
+    public float speed = 6f;
+    public Transform cam; // camera
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        cam = GameObject.Find("Main Camera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal"); // outputs -1 or 1.   -1 = A or Left arrow key. 1 for D or right arrow
+        float vertical = Input.GetAxisRaw("Vertical"); // outputs -1 or 1.   -1 = W key or up arrow. 1 = S key or down arrow.
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            // we have input to move
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; // used to rotate character towards movement
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // Move character
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+}
